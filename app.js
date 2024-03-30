0629ef253516e589f2b30a83345810d701d6b27f
muhammadayaz354 <160526605+muhammadayaz354@users.noreply.github.com>
2024-03-30 22:36:57 +0500

    simple todos list

---
+++ app.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "please type anything You want to add in todos",
+        },
+        {
+            name: "addTodo",
+            type: "confirm",
+            message: "You want to add anyhing more in todos",
+            default: "false",
+        },
+    ]);
+    todos.push(todoQuestion.todo);
+    loop = todoQuestion.addTodo;
+    console.log(todos);
+}
